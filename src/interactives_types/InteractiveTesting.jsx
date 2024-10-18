76158d3fabacbc742a68e9cea75d5eb9848f1822
Tatiana Koroleva <87785060+tatyanakoroleva00@users.noreply.github.com>
2024-10-18 17:00:23 +0300

    testing is done

--- src/interactives_types/InteractiveTesting.jsx
+++ src/interactives_types/InteractiveTesting.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import styles from '../css/InteractiveTesting.module.css';
 
-export default function InteractiveTesting ({click, timeCode, interactivesArr, fullScreen}) {
+export default function InteractiveTesting({ click, timeCode, interactivesArr, fullScreen }) {
+  let data = {}; //тут находится интерактив
 
-  let data = {};
   for (let elem of interactivesArr) {
 
     let episodeTime = elem['time_code'];
@@ -14,133 +14,205 @@ export default function InteractiveTesting ({click, timeCode, interactivesArr, f
     let resultTime = secondsInMinutes + seconds;
 
     if (Math.floor(timeCode) == resultTime) {
-          data = elem;
-      }
-  }
-
-  let testing = data['receivedInfo'];
-
-  //Приведение входящих данных к существующей логике
-  let questionsArr = [];
-
-  for (let key in testing) {
-    let questionObj = {};
-    let answersArr = [];
-
-    for (let internalKey in testing[key]) {
-      if(internalKey === 'question_name') {
-        let questionName = testing[key][internalKey];
-        questionObj['question_name'] = questionName;
-      }
-      if(internalKey === 'correct') {
-        let correctAnswer = testing[key][internalKey];
-        questionObj['correctAnswer'] = correctAnswer;
-        
-      }
-      if (internalKey.includes('answer')) {
-        answersArr.push(testing[key][internalKey]);
-      }
+      data = elem;
     }
-    questionObj.choices = answersArr;
-    questionsArr.push(questionObj);
+  }
+  let testing = data['receivedInfo']['questions'];
+
+  let questions = [];
+
+  for (let question of testing) {
+    let questionObject = {};
+    questionObject['questionText'] = question['question_name'];
+
+    questionObject['answerOptions'] = question['answers'];
+    questions.push(questionObject);
   }
 
-  let totalQuestionsNumber = questionsArr.length;
+  console.log(questions, 'questions');
 
-  const quiz  = {
-    questions : questionsArr,
-    totalQuestions : totalQuestionsNumber,
-    perQuestionScore: 5,
+
+  function toHide() {
+    // //Приведение входящих данных к существующей логике
+    // let questionsArr = [];
+
+
+
+    // for (let key in testing['questions']) {
+
+    //   let questionObj = {};
+    //   let answersArr = [];
+    //   let correctAnswers = [];
+
+
+
+
+
+
+
+    //   for (let internalKey in testing['questions'][key]) {
+
+    //     let testingInteractive = testing['questions'][key];
+
+    //     if (internalKey === 'question_name') {
+    //       let questionName = testing['questions'][key][internalKey];
+    //       questionObj['question_name'] = questionName;
+    //     }
+
+    //     if (internalKey === 'answers') {
+    //       for (let answer of testingInteractive[internalKey]) {
+    //         answersArr.push(answer['name']);
+
+    //         if (answer['status'] === true) {
+    //           correctAnswers.push(answer['name'])
+    //         }
+    //       }
+    //     }
+    //     questionObj.choices = answersArr;
+    //     questionObj['correctAnswer'] = correctAnswers;
+    //     questionsArr.push(questionObj);
+    //   }
+    // }
+
+    // console.log(questionsArr, 'questionsArr');
+
+    // let totalQuestionsNumber = questionsArr.length;
+
+    // const quiz = {
+    //   questions: questionsArr,
+    //   totalQuestions: totalQuestionsNumber,
+    //   perQuestionScore: 5,
+    // };
+
+
+    // const [activeQuestion, setActiveQuestion] = useState(0)
+    // const [selectedAnswer, setSelectedAnswer] = useState('')
+    // const [showResult, setShowResult] = useState(false)
+    // const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null)
+    // const [result, setResult] = useState({
+    //   score: 0,
+    //   correctAnswers: 0,
+    //   wrongAnswers: 0,
+    // });
+
+    // const { questions } = quiz;
+    // const { question_name, choices, correctAnswer } = questions[activeQuestion];
+
+    // const onClickNext = () => {
+    //   setSelectedAnswerIndex(null)
+    //   setResult((prev) =>
+    //     selectedAnswer
+    //       ? {
+    //         ...prev,
+    //         score: prev.score + 5,
+    //         correctAnswers: prev.correctAnswers + 1,
+    //       }
+    //       : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
+    //   )
+    //   if (activeQuestion !== questions.length - 1) {
+    //     setActiveQuestion((prev) => prev + 1)
+    //   } else {
+    //     setActiveQuestion(0)
+    //     setShowResult(true)
+    //   }
+    // };
+
+    // const onAnswerSelected = (answer, index) => {
+    //   setSelectedAnswerIndex(index);
+    //   let chosenAnswer = index + 1;
+
+
+    //   if (chosenAnswer === Math.floor(correctAnswer)) {
+    //     setSelectedAnswer(true)
+    //   } else {
+    //     setSelectedAnswer(false)
+    //   }
+    // };
+
+    // const addLeadingZero = (number) => (number > 9 ? number : `0${number}`);
+  }
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedAnswers, setSelectedAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const maxScore = 5 * questions.length;
+
+  const handleAnswerToggle = (index) => {
+    setSelectedAnswers((prevSelected) => {
+      if (prevSelected.includes(index)) {
+        return prevSelected.filter((i) => i !== index);
+      } else {
+        return [...prevSelected, index];
+      }
+    });
   };
 
-  const [activeQuestion, setActiveQuestion] = useState(0)
-  const [selectedAnswer, setSelectedAnswer] = useState('')
-  const [showResult, setShowResult] = useState(false)
-  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null)
-  const [result, setResult] = useState({
-    score: 0,
-    correctAnswers: 0,
-    wrongAnswers: 0,
-  })
+  const handleNextQuestion = () => {
+    const correctAnswers = questions[currentQuestion].answerOptions
+      .map((option, index) => (option.status ? index : null))
+      .filter((index) => index !== null);
 
-  const { questions } = quiz
-  const { question_name, choices, correctAnswer } = questions[activeQuestion]
+    const isCorrect = correctAnswers.length === selectedAnswers.length &&
+      correctAnswers.every((val) => selectedAnswers.includes(val));
 
-  const onClickNext = () => {
-    setSelectedAnswerIndex(null)
-    setResult((prev) =>
-      selectedAnswer
-        ? {
-            ...prev,
-            score: prev.score + 5,
-            correctAnswers: prev.correctAnswers + 1,
-          }
-        : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
-    )
-    if (activeQuestion !== questions.length - 1) {
-      setActiveQuestion((prev) => prev + 1)
-    } else {
-      setActiveQuestion(0)
-      setShowResult(true)
+    if (isCorrect) {
+      setScore(score + 5);
     }
-  }
 
-  const onAnswerSelected = (answer, index) => {
-    setSelectedAnswerIndex(index);
-    let chosenAnswer = index + 1;
-   if (chosenAnswer === Math.floor(correctAnswer)) {
-      setSelectedAnswer(true)
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+      setSelectedAnswers([]);
     } else {
-      setSelectedAnswer(false)
+      setShowScore(true);
     }
-  }
-
-  const addLeadingZero = (number) => (number > 9 ? number : `0${number}`)
+  };
 
   return (
-    <div className={`${fullScreen  ? styles['container-fullscreen'] : styles['quiz-container']}`}>
-      {!showResult ? (
-        <div>
-          <div><button className={styles['hide-interactive-btn']} onClick={click}> X </button></div>
-          <div>
-            <span className={styles["active-question-no"]}>{addLeadingZero(activeQuestion + 1)}</span>
-            <span className={styles["total-question"]}>/{addLeadingZero(questions.length)}</span>
-          </div>
-          <h2>{question_name}</h2>
-          <ul>
-            {choices.map((answer, index) => (
-              <li
-                onClick={() => onAnswerSelected(answer, index)}
-                key={answer + index}
-                className={selectedAnswerIndex === index ? styles['selected-answer'] : null}>
-                {answer}
-              </li>
-            ))}
-          </ul>
-          <div className={styles["flex-right"]}>
-            <button className={styles['next-btn']} onClick={onClickNext} disabled={selectedAnswerIndex === null}>
-              {activeQuestion === questions.length - 1 ? 'Завершить' : 'Далее'}
-            </button>
-          </div>
-        </div>
-      ) : (
+
+    <div className={`${fullScreen ? styles['container-fullscreen'] : styles['quiz-container']}`}>
+      {showScore ? (
+
         <div className={styles.result}>
           <h2>Результат</h2>
           <p>
             Количество вопросов: <span>{questions.length}</span>
           </p>
           <p>
-            Итог:<span> {result.score}</span>
-          </p>
-          <p>
-            Правильные ответы:<span> {result.correctAnswers}</span>
-          </p>
-          <p>
-            Неправильные ответы:<span> {result.wrongAnswers}</span>
+            Итог:<span> {score} / {maxScore}</span>
           </p>
           <button className={styles['continue-btn']} onClick={click}>Продолжить</button>
         </div>
-      )}
+      ) : (
+        <>
+          <div className='question-section'>
+            <div className='question-count'>
+              <span>Вопрос {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className='question-text'>{questions[currentQuestion].questionText}</div>
+          </div>
+          <ul className='answer-section'>
+            {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+              <li key={index}>
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={selectedAnswers.includes(index)}
+                    onChange={() => handleAnswerToggle(index)}
+                  />
+                  {answerOption.name}
+                </label>
+              </li>
+            ))}
+          </ul>
+
+          <div className={styles["flex-right"]}>
+            <button className={styles['next-btn']} onClick={handleNextQuestion} disabled={selectedAnswers.length === 0}> {currentQuestion === questions.length - 1 ? 'Завершить' : 'Далее'}
+            </button>
+          </div>
+        </>)}
     </div>
+
   )
 }
